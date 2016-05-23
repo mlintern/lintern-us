347c29cc13b7aa390c7654ff5aa12791334427a4
Mark Lintern <mark.lintern@oracle.com>
2016-05-23 14:25:08 -0400

    security updates

--- scripts/popuppolaroid.js
+++ scripts/popuppolaroid.js
@@ -1,47 +1,45 @@
 $(document).ready(function() {
-	$('.little-polaroid').live("click", function() {
-		
+	$('.little-polaroid').on("click", function() {
+
 		width = document.body.offsetWidth;
-		
+
 		if ( width > 630 ){
-		
-		currentID = $(this).attr("id");
-		$(this).css({'visibility' : 'hidden'});
-		
-		//Getting the variable's value from a link 
-		var ImageSource = $(this).find("img").attr('src');
-		
-		//Set the center alignment padding + border see css style
-		var popMargTop = (550 + 24) / 2; 
-		var popMargLeft = (500 + 24) / 2; 
+			currentID = $(this).attr("id");
+			$(this).css({'visibility' : 'hidden'});
 
-		var PolaroidDiv = $('<div>')
-					.attr('class','big-polaroid')
-					.attr('id',currentID)
-					.css({ 
-						'margin-top' : -popMargTop,
-						'margin-left' : -popMargLeft
-					})
-					.append(
-					$('<div>')
-						.attr('class','exit'))
-					.append(
-					$('<div>')
-						.attr('class','inner')
-						.append(
-						$('<img>')
-							.attr('class','large')
-							.attr('src', ImageSource )
-				));
+			//Getting the variable's value from a link 
+			var ImageSource = $(this).find("img").attr('src');
 
-		$('body').append(PolaroidDiv);
+			//Set the center alignment padding + border see css style
+			var popMargTop = (550 + 24) / 2; 
+			var popMargLeft = (500 + 24) / 2; 
+
+			var PolaroidDiv = $('<div>')
+				.attr('class','big-polaroid')
+				.attr('id',currentID)
+				.css({ 
+					'margin-top' : -popMargTop,
+					'margin-left' : -popMargLeft
+				})
+				.append(
+				$('<div>')
+					.attr('class','exit'))
+				.append(
+				$('<div>')
+					.attr('class','inner')
+					.append(
+					$('<img>')
+						.attr('class','large')
+						.attr('src', ImageSource )
+			));
+			$('body').append(PolaroidDiv);
+
+			PolaroidDiv.find('.exit').on("click", function(){
+				currentID = $(this).parent().attr("id");
+				$("#"+currentID).css({'visibility':'visible'});
+				$(this).parent().remove();
+			});
 		}
 	});
 
-	$('.exit').live("click touchstart", function(){
-	currentID = $(this).parent().attr("id");
-	$("#"+currentID).css({'visibility':'visible'});
-	$(this).parent().remove();
-	});
-	
 });
